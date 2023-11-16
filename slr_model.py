5698839749256ae7b493c1c0a492af70b5f215ea
RamananSJ <127139413+RamananSJ@users.noreply.github.com>
2023-11-17 01:56:10 +0530

    Create slr_model.py

---
+++ slr_model.py
@@ -0,0 +1,36 @@
+#without dataset
+import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
+x = np.array([1,2,3,4])
+y = np.array([1,3,4,8])
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+print(slope)
+print(intercept)
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.show()
+
+#with dataset
+import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
+x = np.array([1,2,3,4])
+y = np.array([1,3,4,8])
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+print(slope)
+print(intercept)
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.show()
+

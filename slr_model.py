e54217eb7dd8d2d98537421c0a0ed0e948f8532c
RamananSJ <127139413+RamananSJ@users.noreply.github.com>
2023-11-17 01:57:05 +0530

    Update slr_model.py

--- slr_model.py
+++ slr_model.py
@@ -20,8 +20,8 @@ plt.show()
 import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
-x = np.array([1,2,3,4])
-y = np.array([1,3,4,8])
+import pandas as pd
+
 slope, intercept, r, p, std_err = stats.linregress(x, y)
 print(slope)
 print(intercept)
